c39a3a410271c2d3f4a69a7ab82b1c095c92eaa0
ericdaher <ericdaher2@gmail.com>
2025-05-20 21:01:32 -0300

    add users

---
+++ test/models/user_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  test "should not save user without email_address" do
+    user = User.new(password: "123456", password_confirmation: "123456")
+    assert_not user.save
+  end
+
+  test "should not save user with same email_address as other user" do
+    User.create(email_address: "user@mail.com", password: "123456", password_confirmation: "123456")
+    user = User.new(email_address: "user@mail.com", password: "123456", password_confirmation: "123456")
+    assert_not user.save
+  end
+
+  test "email_address should match the email regex" do
+    user = User.new(email_address: "user", password: "123456", password_confirmation: "123456")
+    assert_not user.save
+  end
+end
